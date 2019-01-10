54de52b0a72d2dd35d18fe6bf48f80eab28323d0
Jennifer Parra <jparra.biblio@gmail.com>
2019-01-10 13:10:00 -0300

    eliminamos código muerto y eliminamos carpeta de bootstrap

--- data.js
+++ data.js
@@ -1,19 +1,14 @@
-
 //Función donde recorreré el arreglo de objetos para acceder a su propiedad name
 //Función para filtrar
-
 const country = () => {
+  let arrayCountryA = [];
   let arrayCountryB = [];
-  let arrayCountryA = []
   let listadoCountry = [];
 
   let route = window.WORLDBANK;
   for (let property in route) {
-
     let routeInitial = route[property].indicators[0]; //así se debe indicar la llamada para que no de error
-    // let routeSecond =routeInitial.countryCode;
     let routeThird = routeInitial.countryName;
-
     arrayCountryB.push(routeThird);
     arrayCountryA.push(route[property]);
   }
@@ -22,16 +17,10 @@ const country = () => {
 }
 window.country = country;
 
-
-
-
-
-
-
-
-const indicatorsNames = (searchCountry) => {   //searchCountry (condición)//
-  let names = []; // arreglo vacío donde pushearé los nombres de indicadores.
-  for (let i = 0; i < searchCountry.indicators.length; i++) { //searchCountry
+//Función que trae todos los indicadores y genera un array con ellos
+const indicatorsNames = (searchCountry) => { //searchCountry (condición)
+  let names = []; //arreglo vacío donde pushearé los nombres de indicadores
+  for (let i = 0; i < searchCountry.indicators.length; i++) { 
     let nameIndicator = searchCountry.indicators[i];
     names.push(nameIndicator.indicatorName);
   }
@@ -40,33 +29,31 @@ const indicatorsNames = (searchCountry) => {   //searchCountry (condición)//
 window.indicatorsNames = indicatorsNames;
 
 
-//Filtrar por país y nombre del indicador
-const dataForYear = (searchCountry, nameIndicator, almacenarObjetoData) => {
+//Función que filtra por país y nombre del indicador
+const dataForYear = (almacenarObjetoData) => {
   let otherObject = [];
   for (let k = 1960; k <= 2017; k++) { //aquí está buscando el atributo dentro de data (que está dentro de otra data)
     let lookYear = (almacenarObjetoData[k]);
     if (lookYear === "") {
       continue; //si pasa eso que no haga nada
     } else {
-      let newObject = { year: 0, valor: 0 };  //inicializando un objeto
+      let newObject = {
+        year: 0,
+        valor: 0
+      }; //inicializando un objeto
       newObject.year = k;
       newObject.valor = lookYear;
       otherObject.push(newObject);
     }
   }
-
   return otherObject; //retorno el arreglo de objetos para luego tomarlo desde el archivo main.js y hacer la visualización de datos con el DOM
 }
 window.dataForYear = dataForYear;
-
-
 
 // FUNCION QUE ORDENA
 const orderDataForYear = (retornoDatosYear) => {
 
   let orderedArray = retornoDatosYear;
-
-
   orderedArray.sort(function (a, b) {
     if (a.year > b.year) {
       return -1;
@@ -76,15 +63,10 @@ const orderDataForYear = (retornoDatosYear) => {
     }
     // a must be equal to b
     return 0;
-
   });
-
   return orderedArray; //retorno el arreglo de objetos para luego tomarlo desde el archivo main.js y hacer la visualización de datos con el DOM
 }
 window.orderDataForYear = orderDataForYear;
-
-
-
 
 // FUNCION QUE CALCULA EL PROMEDIO DE LOS DATOS
 
